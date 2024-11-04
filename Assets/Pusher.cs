2f8b8debb330fa872db272758e6ee2d55e464d18
Jung Who Nam <jungwhonam@gmail.com>
2024-11-04 18:39:33 +0900

    Add pushers

---
+++ Assets/Pusher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    public float pushForce = 10f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // 부딪친 게임 오브젝트 이름
+        string name = collision.gameObject.name;
+        // 부딪친 게임 오브젝트의 RigidBody 컴포넌트
+        Rigidbody rb = collision.rigidbody;
+        // 부딪친 부분의 법선벡터 (그림 참조)
+        Vector3 normalVector = collision.GetContact(0).normal;      
+
+        if (collision.gameObject.name.Equals("Capsule"))
+        {
+            collision.rigidbody.AddForce(-normalVector * pushForce, ForceMode.Impulse);
+        }
+    }
+}
